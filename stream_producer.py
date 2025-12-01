502c634fc3ef249592d4fc57373d03291f9681bf
zainanis <zainanis28862@gmail.com>
2025-12-01 23:12:52 +0500

    made changes to producer

--- stream_producer.py
+++ stream_producer.py
@@ -1,6 +1,8 @@
+
 import pandas as pd
 from datetime import datetime
 from confluent_kafka import Producer
+import time
 
 orders = pd.read_csv("dataset/dynamic/olist_orders_dataset.csv")
 items = pd.read_csv("dataset/dynamic/olist_order_items_dataset.csv")
@@ -19,6 +21,8 @@ batch_size = 2000
 
 
 for i in range(0, len(order_ids), batch_size):
+    
+
     batch_order_ids = order_ids[i:i+batch_size]
     
     # Filter batch
@@ -28,10 +32,11 @@ for i in range(0, len(order_ids), batch_size):
     batch_reviews = reviews[reviews["order_id"].isin(batch_order_ids)]
     
     # Convert to CSV string
-    json_orders = batch_orders.to_json(index=False)
-    json_items = batch_items.to_json(index=False)
-    json_payments = batch_payments.to_json(index=False)
-    json_reviews = batch_reviews.to_json(index=False)
+    json_orders = batch_orders.to_json(orient="records")
+    json_items = batch_items.to_json(orient="records")
+    json_payments = batch_payments.to_json(orient="records")
+    json_reviews = batch_reviews.to_json(orient="records")
+    print(json_orders)
 
     producer.produce('orders',key=f'batch_{i//batch_size +1}',value=json_orders)
     producer.produce('order_items',key=f'batch_{i//batch_size +1}',value=json_items)
@@ -40,3 +45,4 @@ for i in range(0, len(order_ids), batch_size):
 
     producer.flush() 
     print(f"Batch {i//batch_size + 1}: sent {len(batch_order_ids)} orders to Kafka topics")
+    time.sleep(60)
